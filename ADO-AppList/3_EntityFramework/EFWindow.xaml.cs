39b448a71db365d9b505a30498569cb120d765b4
Alexander Vannovskiy <sashavannovski@gmail.com>
2022-11-22 15:18:56 +0200

    Homework 4

--- ADO-AppList/3_EntityFramework/EFWindow.xaml.cs
+++ ADO-AppList/3_EntityFramework/EFWindow.xaml.cs
@@ -10,9 +10,17 @@ namespace ADO_AppList._3_EntityFramework
     /// </summary>
     public partial class EFWindow : Window
     {
+        #region Variables
+
         private EF.FirmContext _firmContext;
         public ObservableCollection<Entities.Department> Departments { get; }
         public ObservableCollection<Entities.Product> Products { get; }
+        public ObservableCollection<Entities.Manager> Managers { get; }
+
+        #endregion
+
+
+        #region Constructor
 
         public EFWindow()
         {
@@ -20,34 +28,45 @@ namespace ADO_AppList._3_EntityFramework
             _firmContext = new();
             Departments = new();
             Products = new();
+            Managers = new();
             this.DataContext = this;
         }
 
+        #endregion
+
+
+        #region Events
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             LabelDepartments.Content = _firmContext.Departments.Count();
-
-            var cntQuery = from p in _firmContext.Products
-                           where p.Price > 0
-                           select p;
-            LabelProducts.Content = cntQuery.Count();
+            LabelManagers.Content = _firmContext.Managers.Count();
+            LabelProducts.Content = _firmContext.Products.Count();
 
             var depQuery = from d in _firmContext.Departments
                            where d.Name != null
                            orderby d.Name descending
                            select d;
-            foreach(var dep in depQuery)
+
+            var productsQuery = _firmContext.Products.Where(p => p.Price > 0)
+                                                     .OrderBy(p => p.Name);
+
+            var managersQuery = _firmContext.Managers.OrderBy(p => p.Name);
+
+            foreach (var dep in depQuery)
             {
                 Departments.Add(dep);
             }
-
-            var query = _firmContext.Products.Where(p => p.Price > 0)
-                                             .OrderBy(p => p.Name);
-            foreach (var p in query)
+            foreach (var p in productsQuery)
             {
                 Products.Add(p);
             }
+            foreach (var m in managersQuery)
+            {
+                Managers.Add(m);
+            }
         }
+
+        #endregion
     }
 }
